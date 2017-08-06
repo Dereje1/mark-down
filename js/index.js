b60b99a142ecad792f4ba0471128c5286bc265dd
Dereje1 <getahun.d@gmail.com>
2017-08-06 19:28:48 -0400

    added github markup

--- js/index.js
+++ js/index.js
@@ -2,21 +2,24 @@ class InputBox extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      inputText:''
+      inputText:'',
+      outputText:''
     }
     //critical otherwise this is not recognized in the function trying to set
     this.updateInput = this.updateInput.bind(this);
   }
   updateInput(ev){
     var typedText = ev.target.value;
+    var chcode = typedText.charCodeAt((typedText.length-1))
     this.setState({inputText:typedText});
+    this.setState({outputText:performMarking(typedText)});
   }
+
   render(){
     return(
       <div>
-        <ConvertedMessage userinput={this.state.inputText} />
-        <textarea onChange={this.updateInput} rows="4" cols="50">
-        </textarea>
+        <textarea onChange={this.updateInput} rows="4" cols="50"></textarea>
+        <div id ="markedUp"></div>
       </div>
     )
   }
@@ -24,7 +27,7 @@ class InputBox extends React.Component {
 
 class ConvertedMessage extends React.Component{
   render(){
-    return <h1>{this.props.userinput}</h1>
+    return <div>{this.props.userinput}</div>
   }
 }
 
@@ -32,3 +35,21 @@ ReactDOM.render(
   <InputBox />,
   document.getElementById('app')
 )
+
+
+//outside function testing()
+function performMarking(someparam){
+  marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
+var markedArea = document.getElementById("markedUp");
+markedArea.innerHTML = marked(someparam);
+return marked(someparam)
+}
