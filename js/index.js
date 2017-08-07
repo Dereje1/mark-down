6452624aac36c658a1a963489c80cf8a93951845
Dereje1 <getahun.d@gmail.com>
2017-08-07 13:28:24 -0400

    final update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+//no need for import as I am not using the production package
 class InputBox extends React.Component {
+  //standard always include
   constructor(props) {
     super(props);
+    //I don't really need state for this project but good practice
     this.state={
       inputText:'',
       outputText:''
@@ -8,48 +11,50 @@ class InputBox extends React.Component {
     //critical otherwise this is not recognized in the function trying to set
     this.updateInput = this.updateInput.bind(this);
   }
+  //updates on change of text area
   updateInput(ev){
+    //note this captures everything in text box not just change
     var typedText = ev.target.value;
+    //just incase I need the character change
     var chcode = typedText.charCodeAt((typedText.length-1))
+    //set state will need for future but not really for this project
+    //as performMarking directly injects marking into div
     this.setState({inputText:typedText});
     this.setState({outputText:performMarking(typedText)});
   }
-
+  //standard always include render function
   render(){
     return(
+      //returns just the one input area div
       <div>
-        <textarea onChange={this.updateInput} rows="4" cols="50"></textarea>
-        <div id ="markedUp"></div>
+        <textarea className = "textarea" onChange={this.updateInput}></textarea>
       </div>
     )
   }
 }
 
-class ConvertedMessage extends React.Component{
-  render(){
-    return <div>{this.props.userinput}</div>
-  }
-}
-
+//standard must always include
 ReactDOM.render(
   <InputBox />,
   document.getElementById('app')
-)
+);
 
 
-//outside function testing()
+//outside function performMarking() does the mardown on inputText
 function performMarking(someparam){
-  marked.setOptions({
-  renderer: new marked.Renderer(),
-  gfm: true,
-  tables: true,
-  breaks: true,
-  pedantic: false,
-  sanitize: false,
-  smartLists: true,
-  smartypants: false
-});
-var markedArea = document.getElementById("markedUp");
-markedArea.innerHTML = marked(someparam);
-return marked(someparam)
+    //options from documentation of library
+    marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: true,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false
+  });
+  //inject marking
+  var markedArea = document.getElementById("markedUp");
+  markedArea.innerHTML = marked(someparam);
+  return marked(someparam);
 }
